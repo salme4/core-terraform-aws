477cc1af1c510dadc6579b69834d067cea63e939
김대현Daehyun kim <daehyun.kim@bespinglobal.com>
2021-09-09 16:03:21 +0900

    china

--- variables.tf
+++ variables.tf
@@ -1,31 +1,31 @@
- variable "ec2_name" {
-   description = "ec2 instance tag Name"
-   default = "demo-edit-core-terraform-ec2"
- }
+variable "ec2_name" {
+  description = "ec2 instance tag Name"
+  default = "china-terraform-core-ec2"
+}
 
- variable "subnet_id" {
-   description = "subnet identity"
-   default = "subnet-0f0d20e44d0cf905a"
- }
+variable "subnet_id" {
+  description = "subnet identity"
+  default = "subnet-03f7963d2c8889cd5"
+}
 
- variable "security_groups" {
-   description = "security group ids"
-   type = list(string)
-   default = ["sg-03c12140254b38dc3"]
- }
+variable "security_groups" {
+  description = "security group ids"
+  type = list(string)
+  default = ["sg-067ee7595b22f17f8"]
+}
 
- variable "key_pair" {
-   description = "pem key pair name"
-   default = "devos-seoul-keypair"
- }
+variable "key_pair" {
+  description = "pem key pair name"
+  default = "devos-chaina-service"
+}
 
- variable "instance_type" {
-   description = "ec2 instance type"
-   default = "t2.micro"
- }
+variable "instance_type" {
+  description = "ec2 instance type"
+  default = "t2.micro"
+}
 
- variable "assign_ip" {
-   description = "use assign ip"
-   type = bool
-   default = true
- }
+variable "assign_ip" {
+  description = "use assign ip"
+  type = bool
+  default = true
+}
