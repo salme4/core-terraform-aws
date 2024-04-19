6ed219f5d6c87c1c8e5d322a9812250ea0118fa9
daveKim <48761100+salme4@users.noreply.github.com>
2024-04-19 11:01:23 +0900

    rollback

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "ec2_instance" {
+  most_recent = true
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["${data.aws_caller_identity.current.account_id}"]
+}
+
 resource "aws_instance" "aws-ec2" {
-  ami = ami-0d3d9b94632ba1e57
+  ami = data.aws_ami.ec2_instance.id
   instance_type = var.instance_type
   associate_public_ip_address = var.assign_ip
   key_name = var.key_pair
